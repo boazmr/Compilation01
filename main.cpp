aed263d3a7707347b5de9732e4fd816870297906
Michaelbeygel <michaelbeygel@gmail.com>
2025-04-21 16:04:48 +0300

    last version for real

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ void handle_string(char *s)
     {
         if (lexema[i] == '\\')
         {
-            if(i + 1 == len)
+            if (i + 1 == len)
             { // '\' the last charcater in the string.
                 output::errorUnclosedString();
             }
@@ -43,12 +43,12 @@ void handle_string(char *s)
             case 'x':
                 if (i + 1 == len) // no place for hex values example: "aa\x"
                 {
-                    char undefined_escape[1] = {'x'};
+                    char undefined_escape[2] = {'x', '\0'};
                     output::errorUndefinedEscape(undefined_escape);
                 }
                 else if (i + 2 == len) // place for only 1 hex value, example: "aa\x2"
                 {
-                    char undefined_escape[2] = {'x' ,lexema[i + 1]};
+                    char undefined_escape[3] = {'x', lexema[i + 1], '\0'};
                     output::errorUndefinedEscape(undefined_escape);
                 }
                 else
@@ -62,11 +62,14 @@ void handle_string(char *s)
                     hexStr += lexema[i + 1];
                     hexStr += lexema[i + 2];
                     char result;
-                    try{
+                    try
+                    {
                         result = static_cast<char>(std::stoi(hexStr, nullptr, 16));
-                    } catch (...) {
+                    }
+                    catch (...)
+                    {
                         // In case this function fail if the digits are not hexa digits. For example: lexema[i + 1] == 'P'
-                        char undefined_escape[3] = {'x' ,lexema[i + 1], lexema[i + 2]};
+                        char undefined_escape[4] = {'x', lexema[i + 1], lexema[i + 2], '\0'};
                         output::errorUndefinedEscape(undefined_escape);
                     }
                     // Result is a hex value character which value is the value after \x..
@@ -77,13 +80,13 @@ void handle_string(char *s)
                     }
                     else
                     {
-                        char undefined_escape[3] = {'x', lexema[i + 1], lexema[i + 2]};
+                        char undefined_escape[4] = {'x', lexema[i + 1], lexema[i + 2], '\0'};
                         output::errorUndefinedEscape(undefined_escape);
                     }
                 }
                 break;
             default: // there is \ and after this nothing familier
-                char undefined_escape[1] = {lexema[i]};
+                char undefined_escape[2] = {lexema[i], '\0'};
                 output::errorUndefinedEscape(undefined_escape);
                 break;
             }
