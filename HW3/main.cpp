649078975dd027d73b8a3ec98b9c4556508f567c
michaelbeygel <michaelbeygel@gmail.com>
2025-05-27 18:03:00 +0300

    HW# init

---
+++ HW3/main.cpp
@@ -0,0 +1,25 @@
+#include "output.hpp"
+#include "nodes.hpp"
+
+// Extern from the bison-generated parser
+extern int yyparse();
+extern int yylineno;
+
+// Basic implementation of yyerror.
+ void yyerror (char const *s) {
+   output::errorSyn(yylineno);
+ }
+
+extern std::shared_ptr<ast::Node> program;
+
+int main() {
+    // Parse the input. The result is stored in the global variable `program`
+    yyparse();
+    // Print the AST using the PrintVisitor
+    if(program){
+      output::SemanticVisitor semanticVisitor;
+      program->accept(semanticVisitor);
+      semanticVisitor.first_round = false;
+      program->accept(semanticVisitor);
+    }
+}
--- HW3/output.hpp
+++ HW3/output.hpp
@@ -63,6 +63,109 @@ namespace output {
 
         friend std::ostream &operator<<(std::ostream &os, const ScopePrinter &printer);
     };
+
+enum Type{}
+
+struct Type_Offset{
+    Type type;
+    int offset;
+}
+
+class SymbolTable
+{
+    private:
+        std::map<std::string, Type_Offset> symbolTable;
+
+    public:
+        SymTable();
+}
+
+/* PrintVisitor class
+     * This class is used to print the AST in a human-readable format.
+     */
+    class SemanticVisitor : public Visitor {
+    private:
+        std::stack<SymbolTable> symbol_stack;
+        std::stack<int> offsets;
+        /* Helper function to print a string with the current indentation */
+        void print_indented(const std::string &str);
+
+        /* Functions to manage the indentation level */
+        void enter_child();
+
+        void enter_last_child();
+
+        void leave_child();
+
+    public:
+        bool first_run;
+        SemanticVisitor();
+
+        void push_symbol_table();
+        void pop_symbol_table();
+        bool search_var(std::string &name);
+        void push_var(std::string &name, Type type, int offset);
+
+        void visit(ast::Num &node) override;
+
+        void visit(ast::NumB &node) override;
+
+        void visit(ast::String &node) override;
+
+        void visit(ast::Bool &node) override;
+
+        void visit(ast::ID &node) override;
+
+        void visit(ast::BinOp &node) override;
+
+        void visit(ast::RelOp &node) override;
+
+        void visit(ast::Not &node) override;
+
+        void visit(ast::And &node) override;
+
+        void visit(ast::Or &node) override;
+
+        // void visit(ast::Type &node) override;
+
+        void visit(ast::ArrayType &node) override;
+
+        void visit(ast::PrimitiveType &node) override;
+
+        void visit(ast::ArrayDereference &node) override;
+
+        void visit(ast::ArrayAssign &node) override;
+
+        void visit(ast::Cast &node) override;
+
+        void visit(ast::ExpList &node) override;
+
+        void visit(ast::Call &node) override;
+
+        void visit(ast::Statements &node) override;
+
+        void visit(ast::Break &node) override;
+
+        void visit(ast::Continue &node) override;
+
+        void visit(ast::Return &node) override;
+
+        void visit(ast::If &node) override;
+
+        void visit(ast::While &node) override;
+
+        void visit(ast::VarDecl &node) override;
+
+        void visit(ast::Assign &node) override;
+
+        void visit(ast::Formal &node) override;
+
+        void visit(ast::Formals &node) override;
+
+        void visit(ast::FuncDecl &node) override;
+
+        void visit(ast::Funcs &node) override;
+    };
 }
 
 #endif //OUTPUT_HPP
